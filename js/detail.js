2842076c3f8abc82fa42cc7bcd23057689ed9ee2
hongxuepeng <125459178@qq.com>
2018-04-20 11:53:45 +0800

    ie

--- js/detail.js
+++ js/detail.js
@@ -228,38 +228,42 @@ Load.prototype = {
     },
     loadMap:function() {
         mapboxgl.accessToken = 'pk.eyJ1IjoieHVlcGVuZ2hvbmciLCJhIjoiY2plcWpuMGllMGEzaTJwcDdoZGl3ZjIwYyJ9.1fVEnVymEmu89XLxUkCDSw';
-        var geojson = {
-            "type": "FeatureCollection",
-            "features": this.featdata
-        };
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v9',
-            center: [-65.017, -16.457],
-            zoom: 5
-        });
-        // add markers to allData
-        geojson.features.forEach(function(marker) {
-            var popup = new mapboxgl.Popup()
-                .setHTML('<a class="mask-content" href="'+marker.properties.url+'" target="_blank"><img src="'+marker.properties.img+'"><div class="NameTips">纽约大学</div></a>');
-            // create a DOM element for the marker
-            var el = document.createElement('div');
-            el.className = 'marker';
-            el.innerHTML =marker.properties.money;
-            el.addEventListener('click', function() {
-                var chapterName={
-                    center: marker.geometry.coordinates,
-                    zoom: 8,
-                    speed: 1.5
-                }
-                map.flyTo(chapterName);
+        if (!mapboxgl.supported()) {
+            $(".search-right,.map-content,.map-title").hide();
+        }else{
+            var geojson = {
+                "type": "FeatureCollection",
+                "features": this.featdata
+            };
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: [-65.017, -16.457],
+                zoom: 5
             });
-            // add marker to allData
-            new mapboxgl.Marker(el)
-                .setLngLat(marker.geometry.coordinates)
-                .setPopup(popup) // sets a popup on this marker
-                .addTo(map);
-        });
+            // add markers to allData
+            geojson.features.forEach(function(marker) {
+                var popup = new mapboxgl.Popup()
+                    .setHTML('<a class="mask-content" href="'+marker.properties.url+'" target="_blank"><img src="'+marker.properties.img+'"><div class="NameTips">纽约大学</div></a>');
+                // create a DOM element for the marker
+                var el = document.createElement('div');
+                el.className = 'marker';
+                el.innerHTML =marker.properties.money;
+                el.addEventListener('click', function() {
+                    var chapterName={
+                        center: marker.geometry.coordinates,
+                        zoom: 8,
+                        speed: 1.5
+                    }
+                    map.flyTo(chapterName);
+                });
+                // add marker to allData
+                new mapboxgl.Marker(el)
+                    .setLngLat(marker.geometry.coordinates)
+                    .setPopup(popup) // sets a popup on this marker
+                    .addTo(map);
+            });
+        }
     },
     pageLoad:function() {
         var GG = {
